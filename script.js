403899198fdc819da937bb5a421a9abf731bd88c
Imani <sabinaimani0@gmail.com>
2026-04-28 11:27:19 +0300

    updated gender in jss

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ function generateName(){
     let year = Number(document.getElementById("year").value);
 
     //2. Validation 
-    if (<1 || day >31){
+    if (day <1 || day >31){
+        alert("Day must be between 1 and 31");
+        return;
     }
+    if( month <1 || month >12){
+        alert("Months must be between 12 months")
+    }
+}
+if(!gender){
+    alert("Please select a gender");
+    return;
 }
