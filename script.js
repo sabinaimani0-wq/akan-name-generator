632fd566ab2c8c1fffdf2e1d44958cb0a4df7d67
Imani <sabinaimani0@gmail.com>
2026-04-28 11:17:03 +0300

    upated javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+function generateName(){
+    //1. Get Input
+    let day = Number(document.getElementById("day").value);
+    let month = Number(document.getElementById("month").value);
+    let year = Number(document.getElementById("year").value);
+
+    //2. Validation 
+    if (<1 || day >31){
+    }
+}
